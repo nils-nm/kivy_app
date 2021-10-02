f8be98c8446deb4507ac03df6fc27eddf0d72871
nils-nm <nilsfuruskog@yandex.ru>
2021-10-02 11:33:16 +0300

    correct files

--- study/lession_1.py
+++ study/lession_1.py
@@ -1,8 +1,13 @@
 # screen manager
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+# from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class Container(BoxLayout):
+    pass
 
 
 class FirstScreen(Screen):
@@ -17,12 +22,12 @@ class WindowManager(ScreenManager):
     pass
 
 
-kv = Builder.load_file("lession_1.kv")
+# kv = Builder.load_file("lession_1.kv")
 
 
 class MyApp(App):
     def build(self):
-        return kv
+        return Container()
 
 
 if __name__ == "__main__":
